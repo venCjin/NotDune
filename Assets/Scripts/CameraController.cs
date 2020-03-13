25af905a85486baf07f4b24ed93715cc785c965d
Piotr <jezyna.p@gmail.com>
2020-03-13 01:04:47 +0100

    Initial commit. Project set-up. Simple movement. Camera System.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    public enum CameraMode { AboveGround, UnderGround };
+
+    [SerializeField] private CinemachineVirtualCamera _aboveGroundCamera = null;
+    [SerializeField] private CinemachineVirtualCamera _underGroundCamera = null;
+
+    private bool _isAboveGround = true;
+
+    private void Start()
+    {
+        SetCameraMode(CameraMode.AboveGround);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isAboveGround = !_isAboveGround;
+
+            if (_isAboveGround)
+            {
+                SetCameraMode(CameraMode.AboveGround);
+            }
+            else
+            {
+                SetCameraMode(CameraMode.UnderGround);
+            }
+        }
+    }
+
+    public void SetCameraMode(CameraMode mode)
+    {
+        if (mode == CameraMode.AboveGround)
+        {
+            _aboveGroundCamera.Priority = 100;
+            _underGroundCamera.Priority = 0;
+        }
+        else
+        {
+            _aboveGroundCamera.Priority = 0;
+            _underGroundCamera.Priority = 100;
+        }
+    }
+}
---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnDestroy()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
