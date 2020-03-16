cfe224bff54dbbf26e59f94461078aae30349891
Andrzej Zabski <jedrak32@gmail.com>
2020-03-16 16:49:26 +0100

    Segments

--- Assets/Scripts/Segment.cs
+++ Assets/Scripts/Segment.cs
@@ -4,25 +4,55 @@ using UnityEngine;
 
 public class Segment : MonoBehaviour
 {
-    public float speed;
+    public float AboveGroundSpeed;
+    public float UnderGroundSpeed;
+    private float _speed;
     public Transform Target;
+    private CharacterController _characterController;
 
     [SerializeField]
-    private float _distanceBeetweenSegments = 2;
+    private float _distanceBeetweenSegments = .1f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _speed = AboveGroundSpeed;
+        _characterController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+        _characterController.OnStateChanged += OnStateChange;
+        _distanceBeetweenSegments = .7f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        Vector3 v = Target.position - transform.position;
-        v.Normalize();
-        v *= _distanceBeetweenSegments;
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(Target.position.x - v.x, Target.position.y - v.y, Target.position.z - v.z), step);
+        float step = _speed * Time.deltaTime;
+        if(Vector3.Distance(Target.position, transform.position) > _distanceBeetweenSegments)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, step);
+        }
+        transform.LookAt(Target.position);
+        transform.Rotate(90, 0, 0);
+
+    }
+
+    private void OnStateChange(CharacterController.State state)
+    {
+        
+        if(state == CharacterController.State.AboveGround)
+        {
+            _speed = AboveGroundSpeed;
+           
+        }
+        else
+        {
+            _speed = UnderGroundSpeed;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _characterController.OnStateChanged -= OnStateChange;
     }
 }
